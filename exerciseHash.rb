5d8c09562da2ed16d87b2a152c71540e77b38e3d
Bruno Mota <bmotadev@gmail.com>
2023-02-08 16:30:00 -0300

    exercise hash in ruby

---
+++ exerciseHash.rb
@@ -0,0 +1,3 @@
+alunos = [{ nome: 'Alberto', nota: 7, disciplina: 'Artes'}, {nome: 'Ingrid', nota: 10, disciplina: 'Biologia'}, { nome: 'Bruno', nota: 9, disciplina: "Matemática"}]
+
+puts "#{alunos[0][:nome]} tirou nota #{alunos[0][:nota]} em #{alunos[0][:disciplina]}"
